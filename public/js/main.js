0fc58e6037bf12dbd672eb7e411179ccccdbf738
Shiratie Prodhan <shiratiee@gmail.com>
2017-08-16 17:46:50 -0400

    progress

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,53 @@
 /* global $ tripModule */
 
 $(tripModule.load);
+
+$(document).ready(() => {
+    const $hotelChoices = $('#hotel-choices')
+    const $restaurantChoices = $('#restaurant-choices')
+    const $activityChoices = $('#activity-choices')
+    //this is going to return a promise array of all hotels 
+
+    $.get('/api/hotels')
+        .then(function (response) {
+            response.forEach(function (hotel) {
+                console.log(hotel);
+                $hotelChoices.append(`<option>${hotel.name}</option>`)
+            })
+        })
+        .catch(console.error.bind(console));
+
+    $.get('/api/restaurants')
+        .then(function (response) {
+            console.log("are we here")
+            response.forEach(function (restaurant) {
+                console.log(restaurant);
+                $restaurantChoices.append(`<option>${restaurant.name}</option>`)
+            })
+        })
+        .catch(console.error.bind(console));
+
+    $.get('/api/activities')
+        .then(function (response) {
+            response.forEach(function (activity) {
+                console.log(activity);
+                $activityChoices.append(`<option>${activity.name}</option>`)
+            })
+        })
+        .catch(console.error.bind(console));
+
+
+
+
+    // $.ajax({
+    //   method: 'VERB',
+    //   url: '/whatever/route',
+    //   data: someDataToSend, // e.g. for POST requests
+    // })
+    // .then(function (responseData) {
+    //   // some code to run when the response comes back
+    // })
+    // .catch(function (errorObj) {
+    //   // some code to run if the request errors out
+    // });
+})
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,41 @@ var Hotel = require('../models').Hotel;
 var Restaurant = require('../models').Restaurant;
 var Activity = require('../models').Activity;
 
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   Promise.all([
     Hotel.findAll(),
     Restaurant.findAll(),
     Activity.findAll()
   ])
-  .spread(function(dbHotels, dbRestaurants, dbActivities) {
-    res.render('index', {
-      templateHotels: dbHotels,
-      templateRestaurants: dbRestaurants,
-      templateActivities: dbActivities
-    });
-  })
-  .catch(next);
+    .spread(function (dbHotels, dbRestaurants, dbActivities) {
+      res.render('index', {
+        templateHotels: dbHotels,
+        templateRestaurants: dbRestaurants,
+        templateActivities: dbActivities
+      });
+    })
+    .catch(next);
 });
 
+router.get('/api/hotels', function (req, res, next) {
+  console.log("getting into API")
+  //returns a promise, which is an array of all hotels 
+  Hotel.findAll()
+    .then(function (hotels) {
+      res.send(hotels)
+    })
+})
+router.get('/api/restaurants', function (req, res, next) {
+  Restaurant.findAll()
+    .then(function (restaurants) {
+      res.send(restaurants)
+    })
+})
+router.get('/api/activities', function (req, res, next) {
+  Activity.findAll()
+    .then(function (activities) {
+      res.send(activities)
+    })
+})
 module.exports = router;
